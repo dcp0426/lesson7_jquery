269b1c6bddad6409958aba572ce5a594b7f5a344
dcp0426 <dcp14@my.fsu.edu>
2016-04-14 01:52:42 +0000

    lesson 7 final commit

--- Lesson7_Assignment/drill4.js
+++ Lesson7_Assignment/drill4.js
@@ -15,7 +15,7 @@ function display(event) {
 
 //displays and animates the next element after the current target
 function display2(event) {
-$ (event.currentTarget).next().animate( {height: ‘toggle’}, “slow”);
+$ (event.currentTarget).next().animate( {height: "toggle"}, "slow");
 }//end of display2
 
 //attach event listener to h3 elements to invoke display function when clicked
@@ -29,7 +29,7 @@ $ ("h3").hover(function(){
 });
 
 //hover() will trigger display2 method each time mouse hovers over header
-$ (“h3”).hover(display2);
+$ ("h3").hover(display2);
 
 
 
